af2d57d33b00affb164766a7e00ad022a21e7dcf
Nahuel Garbezza <n.garbezza@gmail.com>
2023-01-26 23:03:22 -0300

    refactor: helper function to create db top level structure

--- test/ghost_dump_parser_test.js
+++ test/ghost_dump_parser_test.js
@@ -1,54 +1,40 @@
 const { suite, test, assert } = require('@pmoo/testy');
 const { GhostDumpParser } = require('../src/ghost_dump_parser.js');
 
+const dbWithData = data => {
+  return { db: [ { data } ] }
+}
+
 suite('Ghost dump parser', () => {
   test('it reads author names in the order they appear on the list', () => {
-    const data = {
-      db: [
-        {
-          data: {
-            users: [
-              { name: 'Lionel Messi' },
-              { name: 'Ángel Di María' }
-            ]
-          }
-        }
+    const data = dbWithData({
+      users: [
+        { name: 'Lionel Messi' },
+        { name: 'Ángel Di María' }
       ]
-    }
+    })
     const parser = new GhostDumpParser(data)
     assert.that(parser.userNames()).isEqualTo(['Lionel Messi', 'Ángel Di María'])
   });
 
   test('it reads featured image URLs from the posts section', () => {
-    const data = {
-      db: [
-        {
-          data: {
-            posts: [
-              { feature_image: 'https://example.org/1' },
-              { feature_image: 'https://example.org/2' },
-            ]
-          }
-        }
+    const data = dbWithData({
+      posts: [
+        { feature_image: 'https://example.org/1' },
+        { feature_image: 'https://example.org/2' },
       ]
-    }
+    })
     const parser = new GhostDumpParser(data)
     assert.that(parser.allImageUrls()).isEqualTo(['https://example.org/1', 'https://example.org/2'])
   });
 
   test('it skips null featured image URLs from the posts section', () => {
-    const data = {
-      db: [
-        {
-          data: {
-            posts: [
-              { feature_image: 'https://example.org/1' },
-              { feature_image: null },
-            ]
-          }
-        }
+    const data = dbWithData({
+      posts: [
+        { feature_image: 'https://example.org/1' },
+        { feature_image: null },
       ]
-    }
+    })
     const parser = new GhostDumpParser(data)
     assert.that(parser.allImageUrls()).isEqualTo(['https://example.org/1'])
   });
